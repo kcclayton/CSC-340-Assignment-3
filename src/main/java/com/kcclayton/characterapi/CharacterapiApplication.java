83c0594b0268d3911c08bd9c54e0e1699febc4c7
kcclayton <kcclayton@uncg.edu>
2026-03-04 13:20:31 -0500

    Change class name

--- src/main/java/com/kcclayton/characterapi/CharacterapiApplication.java
+++ src/main/java/com/kcclayton/characterapi/CharacterapiApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CharacterapiApplication {
+public class CharacterApiApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(CharacterapiApplication.class, args);
+		SpringApplication.run(CharacterApiApplication.class, args);
 		
 	}
 
